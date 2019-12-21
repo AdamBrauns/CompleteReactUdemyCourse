d031b5983431a0f1d03110970ffbe94652ae0d26
Adam Brauns <braunsaj14@uww.edu>
2019-12-20 21:54:11 -0600

    made the person component dynamic

--- section_3/react-complete-guide/src/Person/Person.js
+++ section_3/react-complete-guide/src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 // Same as the filename
+// For the js {} part, it needs to be one line expressions like function calls or calculations 
 const person = () => {
-    return <p>I'm a Person!</p>    
+    return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>    
 }
 
 export default person;
