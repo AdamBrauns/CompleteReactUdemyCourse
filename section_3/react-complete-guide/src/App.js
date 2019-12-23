dba3f4005de8cc376a20b3864c5b82e3eec4f106
Adam Brauns <braunsaj14@uww.edu>
2019-12-22 19:48:12 -0600

    using the useState method

--- section_3/react-complete-guide/src/App.js
+++ section_3/react-complete-guide/src/App.js
@@ -1,51 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 // Can omit the js in Person
 import Person from './Person/Person';
 
-class App extends Component {
-  // You can only use this in component that extends Component
-  // State will re-render the DOM
-  state = {
-    persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 28 },
-      { name: 'Stephanie', age: 22 }
-    ],
-    otherState: 'some other value'
-  }
+const app = props => {
+  // useState returns two items (current state and function that allows us to update it)
+  const [ personsState, setPersonsState] = useState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: 'Manu', age: 45 },
+        { name: 'Stephanie', age: 22 }
+      ]
+    });
 
-  switchNameHandler = () => {
+  const [otherState, setOtherState] = useState('');
+  
+  const switchNameHandler = () => {
     // console.log('Was Clicked!');
     // Don't do this:
     // this.state.persons[0].name = 'Maximilian'
 
-    // This will not override otherState
-    this.setState({persons: [
-      { name: 'Maximilian', age: 28 },
-      { name: 'Manu', age: 28 },
-      { name: 'Stephanie', age: 78 }
-    ]})
+    // This will override otherState, need to set otherState now
+    setPersonsState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 60 },
+        { name: 'Stephanie', age: 78 }
+      ],
+    });
+    setOtherState('Some other value')
   }
 
-  render() {
-    return (
-      // Can't use class because it is a js keyword, instead use className
-      // In the end, it gets translated into class
-      // Typically, you want to wrap everything into one element
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-      </div>
-    );
+  return (
+    // Can't use class because it is a js keyword, instead use className
+    // In the end, it gets translated into class
+    // Typically, you want to wrap everything into one element
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      <button onClick={ switchNameHandler }>Switch Name</button>
+      <Person name={ personsState.persons[0].name } age={ personsState.persons[0].age }/>
+      <Person name={ personsState.persons[1].name } age={ personsState.persons[1].age }>My Hobbies: Racing</Person>
+      <Person name={ personsState.persons[2].name } age={ personsState.persons[2].age }/>
+      <p>{ otherState }</p>
+    </div>
+  );
 
-    // This is the same equivalent to the one above
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'))
-  }
+  // This is the same equivalent to the one above
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'))
+
 }
 
-export default App;
+export default app;
