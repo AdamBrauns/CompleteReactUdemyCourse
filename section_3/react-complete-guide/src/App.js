c57a3ac3cf2a2b58d4f507ae5146dd7cfaf7d783
Adam Brauns <braunsaj14@uww.edu>
2019-12-22 20:15:08 -0600

    Passing functions and using the bind

--- section_3/react-complete-guide/src/App.js
+++ section_3/react-complete-guide/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     otherState: 'some other value'
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was Clicked!');
     // Don't do this:
     // this.state.persons[0].name = 'Maximilian'
@@ -21,7 +21,7 @@ class App extends Component {
     // This will override otherState, need to set otherState now
     this.setState({
       persons: [
-        { name: 'Maximilian', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
       ]
@@ -36,7 +36,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -44,6 +44,7 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
         >
           My Hobbies: Racing
         </Person>
