cbf59e307db9dd817e1619f02498c62be8180a6f
Adam Brauns <braunsaj14@uww.edu>
2019-12-22 19:57:58 -0600

    Rolling back to the class based file

--- section_3/react-complete-guide/src/App.js
+++ section_3/react-complete-guide/src/App.js
@@ -1,54 +1,65 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
-// Can omit the js in Person
+// Can omit the js in the Person file
 import Person from './Person/Person';
 
-const app = props => {
-  // useState returns two items (current state and function that allows us to update it)
-  const [ personsState, setPersonsState] = useState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: 'Manu', age: 45 },
-        { name: 'Stephanie', age: 22 }
-      ]
-    });
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  };
 
-  const [otherState, setOtherState] = useState('');
-  
-  const switchNameHandler = () => {
+  switchNameHandler = () => {
     // console.log('Was Clicked!');
     // Don't do this:
     // this.state.persons[0].name = 'Maximilian'
 
     // This will override otherState, need to set otherState now
-    setPersonsState({
+    this.setState({
       persons: [
         { name: 'Maximilian', age: 28 },
-        { name: 'Manu', age: 60 },
-        { name: 'Stephanie', age: 78 }
-      ],
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
     });
-    setOtherState('Some other value')
-  }
+  };
 
-  return (
+  render() {
     // Can't use class because it is a js keyword, instead use className
     // In the end, it gets translated into class
     // Typically, you want to wrap everything into one element
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working!</p>
-      <button onClick={ switchNameHandler }>Switch Name</button>
-      <Person name={ personsState.persons[0].name } age={ personsState.persons[0].age }/>
-      <Person name={ personsState.persons[1].name } age={ personsState.persons[1].age }>My Hobbies: Racing</Person>
-      <Person name={ personsState.persons[2].name } age={ personsState.persons[2].age }/>
-      <p>{ otherState }</p>
-    </div>
-  );
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
 
   // This is the same equivalent to the one above
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'))
 
+
 }
 
-export default app;
+export default App;
