7ccec903a4001ccf2e4b2c28bdba5006f23d1b67
Adam Brauns <braunsaj14@uww.edu>
2019-12-29 17:23:11 -0600

    dynamically doing lists now

--- section_4/react-complete-guide/src/App.js
+++ section_4/react-complete-guide/src/App.js
@@ -14,21 +14,6 @@ class App extends Component {
     showPersons: false
   };
 
-  switchNameHandler = (newName) => {
-    // console.log('Was Clicked!');
-    // Don't do this:
-    // this.state.persons[0].name = 'Maximilian'
-
-    // This will override otherState, need to set otherState now
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    });
-  };
-
   nameChangedHandler = (event) => {
     this.setState({
       persons: [
@@ -58,22 +43,11 @@ class App extends Component {
     if (this.state.showPersons === true) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Max!')}
-            changed={this.nameChangedHandler}
-          >
-            My Hobbies: Racing
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          />
+          {this.state.persons.map(person => {
+            return <Person
+                    name={person.name}
+                    age={person.age} />
+          })}
         </div>
       );
     }
