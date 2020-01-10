6f0822f4b65900ebf553c83ee117183c3ae27500
Adam Brauns <braunsaj14@uww.edu>
2020-01-09 19:59:12 -0600

    added button to remove cockpit

--- section_7/react-complete-guide/src/components/Cockpit/Cockpit.js
+++ section_7/react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -10,9 +10,19 @@ const cockpit = (props) => {
     setTimeout(() => {
       alert('Saved data to cloud!')
     }, 1000);
+    return () => {
+      console.log('[Cockpit.js] cleanup work in useEffect')
+    }
   // this will run whenever props.persons change. [] runs only the first time
   //}, [props.persons]);
   }, []);
+
+  useEffect(() => {
+    console.log('[Cockpit.js] 2nd useEffect');
+    return () => {
+      console.log('[Cockpit.js] cleanup work in 2nd useEffect')
+    };
+  });
 
   // useEffect can be run more than once
   // useEffect();
