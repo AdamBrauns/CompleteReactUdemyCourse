59b407431cdace11cdb4dd21aa55478352f6ad69
Adam Brauns <braunsaj14@uww.edu>
2020-01-09 20:02:14 -0600

    example of cleanup: click remove cockpit before timer goes off

--- section_7/react-complete-guide/src/components/Cockpit/Cockpit.js
+++ section_7/react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -7,10 +7,11 @@ const cockpit = (props) => {
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
     // Http request...
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       alert('Saved data to cloud!')
     }, 1000);
     return () => {
+      clearTimeout(timer);
       console.log('[Cockpit.js] cleanup work in useEffect')
     }
   // this will run whenever props.persons change. [] runs only the first time
