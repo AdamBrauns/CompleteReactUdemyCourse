4176227e72880c9f1ff5c4f50ae659609852b32b
Adam Brauns <braunsaj14@uww.edu>
2020-01-21 19:02:26 -0600

    introducing prop chain proplem on purpose

--- section_7/react-complete-guide/src/Containers/App.js
+++ section_7/react-complete-guide/src/Containers/App.js
@@ -21,7 +21,8 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false,
     showCockpit: true,
-    changeCounter: 0
+    changeCounter: 0,
+    authenticated: false
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -78,6 +79,10 @@ class App extends Component {
     this.setState({ showPersons: !doesShow });
   };
 
+  loginHandler = () => {
+    this.setState({authenticated: true});
+  };
+
   render() {
     console.log('[App.js] render')
     let persons = null;
@@ -87,7 +92,8 @@ class App extends Component {
           <Persons 
             persons={this.state.persons}
             clicked={this.deletePersonHandler}
-            changed={this.nameChangedHandler} />;
+            changed={this.nameChangedHandler}
+            isAuthenticated={this.state.authenticated} />;
     }
 
     return (
@@ -102,7 +108,8 @@ class App extends Component {
           title={this.props.appTitle}
           showPersons={this.state.showPersons}
           personsLength={this.state.persons.length}
-          clicked={this.togglePersonsHandler} />
+          clicked={this.togglePersonsHandler}
+          login={this.loginHandler} />
         ) : null}
         {persons}
       </Auxillary>
