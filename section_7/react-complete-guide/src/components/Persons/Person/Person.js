102995a3cd436941c2b5e4fb6e111a84ac1d9b53
Adam Brauns <braunsaj14@uww.edu>
2020-01-21 18:45:30 -0600

    added createref

--- section_7/react-complete-guide/src/components/Persons/Person/Person.js
+++ section_7/react-complete-guide/src/components/Persons/Person/Person.js
@@ -6,6 +6,17 @@ import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
 
 class Person extends Component {
+
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log('[Person.js] rendering...');
     return (
@@ -15,6 +26,9 @@ class Person extends Component {
         </p>
         <p key="i2">{this.props.children}</p>
         <input
+          key="i3"
+          // ref={(inputEl) => {this.inputElement = inputEl}}
+          ref={this.inputElementRef}
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
