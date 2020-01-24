495df8214b355204733f2c2a50992873fcb12b89
Adam Brauns <braunsaj14@uww.edu>
2020-01-23 21:15:34 -0600

    added layout to the app

--- section_8/burger-builder/src/App.js
+++ section_8/burger-builder/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <div>
-
+      <Layout>
+        <p>Test</p>
+      </Layout>
     </div>
   );
 }
